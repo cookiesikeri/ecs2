e4f929cc5778ac88feee5fb7073da707aa5c06ae
ebenezer <39166121+cookiesikeri@users.noreply.github.com>
2024-08-08 11:31:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/app2', (req, res) => {
+  res.send('Hello World 22222222222!');
 });
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
